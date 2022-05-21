5f9735731cbd9fedcec9c01f229cd68b990829c4
elizabethvera-atieno <atienolizvera@gmail.com>
2022-05-21 15:16:53 +0300

    complete test

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,24 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  
+  return <div className="navbar">
+    <NavLink exact  to="/" >
+      Home
+    </NavLink>
+    <br></br>
+    <NavLink exact  to="/movies" >
+      Movies
+    </NavLink>
+    <br></br>
+    <NavLink exact  to="/directors" >
+      Directors
+    </NavLink>
+    <br></br>
+    <NavLink exact  to="/actors" >
+      Actors
+    </NavLink>
+  </div>;
 }
 
 export default NavBar;
